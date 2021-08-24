d1a94c872147d725407e69cfb14f3e3ee974ddc4
Cathella <ncathie214@gmail.com>
2021-08-24 08:20:16 +0300

    function, arguments

--- testing.rb
+++ testing.rb
@@ -1,5 +1,6 @@
-def hello_world
-  puts "Hello, World"
+def hello_world(greeting = "Hello", person = "Catherine")
+  return "#{greeting}, #{person}"
 end
 
-hello_world
+puts hello_world
+puts hello_world('Hey', 'Tommy')
